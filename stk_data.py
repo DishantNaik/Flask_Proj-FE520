9edd3a2b0f0d11dc5b4a64017dbcf5fc8da3ec58
Dhruv <55866701+dhrupatel97@users.noreply.github.com>
2021-05-03 23:54:48 -0400

    Stock Page Designed

--- stk_data.py
+++ stk_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import datetime as dt
+import pprint
 
 
 def get_data_by_date(stock_code,start_date, end_date):
@@ -8,8 +9,9 @@ def get_data_by_date(stock_code,start_date, end_date):
     d = yf.Ticker(stock_code)
 
     df = d.history(start=start_date, end=end_date)
-
-    return df
+    # pprint.pprint(d.info)
+    df.to_csv('dat.csv')
+    return d.info, df
 
 def get_current_data(stock_code):
 
@@ -19,3 +21,20 @@ def get_current_data(stock_code):
     
     df = d.history(period='1d')
     return df
+
+# get_data_by_date('amzn','2017-04-22','2021-05-02')
+
+# dayHigh 
+# dayLow
+# averageVolumne
+# averageVolumne10days
+# exchangeTimezoneName
+# marketCap
+# ask
+# bid
+# bidSize
+# beta
+# beta3Year
+# open
+# previousClose
+# symbol
